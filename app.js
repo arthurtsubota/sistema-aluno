6b03a13a9f8ceb19231e417c1e193b69f20020ea
arthurtsubota <arthur.tsubota@escola.pr.gov.br>
2026-02-26 12:10:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const NAluno = "arthur toninho"
+const idade = "16"
+const matriculado = false;
+
+if (matriculado == true) {
+    const matricula = "SIM";
+    console.log(`O aluno ${NAluno} tem ${idade} anos. Status de matricula ${matricula}`);
+} else {
+    const matricula = "NÃO";
+    console.log(`O aluno ${NAluno} tem ${idade} anos. Status de matricula ${matricula}`);
+}
